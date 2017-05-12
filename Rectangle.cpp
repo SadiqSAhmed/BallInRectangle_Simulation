a27948d0ad7456152598b8f0622f62292f123bb7
SadiqSAhmed <sadiq.s.ahmed@gmail.com>
2017-05-13 02:41:51 +0600

    Create Rectangle.cpp
    class Rectangle

---
+++ Rectangle.cpp
@@ -0,0 +1,9 @@
+// The rectangle class
+
+class Rectangle (length, width, color) {
+
+    // Attributes
+    int rect_length = length       // length of the rectangle
+    int rect_width  = width        // width of the rectangle
+    
+    
