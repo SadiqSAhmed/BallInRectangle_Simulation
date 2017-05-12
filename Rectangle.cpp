e861829a5b99eea64321ad8074334a4fabad7a73
SadiqSAhmed <sadiq.s.ahmed@gmail.com>
2017-05-13 02:45:36 +0600

    Update Rectangle.cpp

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -3,7 +3,8 @@
 class Rectangle (length, width, color) {
 
     // Attributes
-    int rect_length = length       // length of the rectangle
-    int rect_width  = width        // width of the rectangle
+    int rect_length = length;       // length of the rectangle
+    int rect_width  = width;        // width of the rectangle
     
-    
+    // Methods
+}
